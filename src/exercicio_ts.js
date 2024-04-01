e9f6fd0c82ddd2fc3d5afa6121f801f2aa57af1d
MarceloSilveira <marcelosilverag@hotmail.com>
2024-04-01 11:54:47 -0300

    first commit

---
+++ src/exercicio_ts.js
@@ -0,0 +1,7 @@
+"use strict";
+function calculaArea(base, altura) {
+    return 2 * 5;
+}
+function saudacao(nome) {
+    return 'ola' + nome;
+}
---
+++ src/exercicio_ts.ts
@@ -0,0 +1,7 @@
+function calculaArea(base: number, altura:number): number {
+    return 2 * 5;
+}
+
+function saudacao(nome:string): string {
+    return 'ola' + nome;
+}
